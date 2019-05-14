38299dc86fbd7c46f81af81092f0a08591d817a5
unknown <Michelleweck@web.de>
2019-05-14 19:18:59 +0200

    ListenDesign

--- haw_events_application/lib/EventOverview.dart
+++ haw_events_application/lib/EventOverview.dart
@@ -5,56 +5,58 @@ class EventOverview extends StatelessWidget {
   Widget build(BuildContext context) {
 
     ListTile myListTile() => ListTile(
-      contentPadding: EdgeInsets.symmetric(horizontal: 20.0,vertical: 10.0),
-      leading: Container(
-        padding: EdgeInsets.only(right: 20.0),
-        decoration: new BoxDecoration(
-          border: new Border(
-            right: new BorderSide(width: 1.0, color: Colors.black)
-          )
-        ),
-        child: Icon(Icons.image, color: Colors.black),
+      contentPadding: EdgeInsets.symmetric(horizontal: 8.0,vertical: 10.0),
+      leading: new Container( 
+        margin: EdgeInsets.only(left: 10.0),
+        padding: EdgeInsets.only(),  
+        child: Icon(Icons.image, color: Colors.black, size: 50.0,),
       ),
-      title: new Text(
-        "Eventtitel",
-        style: TextStyle(
-          color: Colors.purple,
-          fontWeight: FontWeight.bold
-        ),
-      ),
-      subtitle: Row(
-        children: <Widget>[
-          Text(
-            "Eventbeschreibung",
-            style: TextStyle(
-              color: Colors.black
-            ),
+      title: new Container(
+        padding: EdgeInsets.only(right: 10, bottom: 5),
+        child: new Text(
+          "Raum -- Datum",
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 15
           ),
-          Text(
-            "Department",
-            style: TextStyle(
-              color: Colors.black
-            ),
-          )
-        ],
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        )
       ),
-      trailing: 
-        Icon(Icons.star_border, color: Colors.white, size: 30.0)
+      subtitle: Container(
+        padding: EdgeInsets.only(bottom: 40),
+        child: new Text(
+          "Veranstaltung",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.black
+          ),
+        )
+      ),
+      trailing: new Container(
+        padding: EdgeInsets.only(left: 10, top: 3),
+        child: Icon(Icons.star_border, color: Color(0xFFa0bedc), size: 42.0)
+      )
     ); 
 
-    final makeCard = new Card(
-      elevation: 8.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20)
+    final makeCard = new Container(
+      height: 100.0,
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+      //padding: EdgeInsets.all(0),
+      decoration: new BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: Colors.white,
+        borderRadius: new BorderRadius.circular(8.0),
+        boxShadow: <BoxShadow>[
+          new BoxShadow(
+            color: Colors.black38,
+            offset: new Offset(0.0, 10.0),
+            blurRadius: 10.0
+          )
+        ]
       ),
-      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white
-        ),
-        child: myListTile(),
-      ),
+      child: new Center(
+        child: myListTile()
+      )
     );
 
     final MyListView = new Container(
@@ -72,6 +74,7 @@ class EventOverview extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.filter),
+        backgroundColor: Color(0xFF003ca0),
       ),
     );
   }
