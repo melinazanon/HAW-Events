9fea66c3e04b77f29a36cd63763b97526d699bae
Makisai <jschmaglowski@aol.com>
2019-05-25 19:00:43 +0200

    Calender View Implemeted(without event functions)

--- haw_events_application/lib/Calender.dart
+++ haw_events_application/lib/Calender.dart
@@ -1,8 +1,79 @@
-import 'package:flutter/material.dart';
+//  Copyright (c) 2019 Aleksander Woźniak
+//  Licensed under Apache License v2.0
 
-class Calender extends StatelessWidget {
+import 'package:date_utils/date_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+
+class Calender extends StatefulWidget {
+  Calender({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _CalenderState createState() => _CalenderState();
+}
+
+class _CalenderState extends State<Calender> with TickerProviderStateMixin {
+  DateTime _selectedDay;
+
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = DateTime.now();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    _controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          // Switch out 2 lines below to play with TableCalendar's settings
+          //-----------------------
+          _buildTableCalendar(),
+          // _buildTableCalendarWithBuilders(),
+          const SizedBox(height: 8.0),
+        ],
+      ),
+    );
+  }
+
+  // Simple TableCalendar configuration (using Styles)
+  Widget _buildTableCalendar() {
+    return TableCalendar(
+      locale: 'en_US',
+      initialCalendarFormat: CalendarFormat.month,
+      formatAnimation: FormatAnimation.slide,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      availableGestures: AvailableGestures.all,
+      availableCalendarFormats: const {
+        CalendarFormat.month: 'Month',
+      },
+      calendarStyle: CalendarStyle(
+        selectedColor: Colors.deepOrange[400],
+        todayColor: Colors.deepOrange[200],
+        markersColor: Colors.brown[700],
+      ),
+      headerStyle: HeaderStyle(
+        formatButtonTextStyle: TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
+        formatButtonDecoration: BoxDecoration(
+          color: Colors.deepOrange[400],
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+      ),
+
+    );
   }
 }
